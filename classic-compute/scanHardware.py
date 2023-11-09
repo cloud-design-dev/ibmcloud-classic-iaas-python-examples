c5e4b4a0c81f802134fd2c41f0ad553992cff08f
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2023-11-09 11:24:15 -0600

    Update scanHardware.py

--- classic-compute/scanHardware.py
+++ classic-compute/scanHardware.py
@@ -1,7 +1,22 @@
+#!/usr/bin/env python3
+# Author: Ryan Tiffany
+# Copyright (c) 2023
+#https://github.com/cloud-design-dev/ibmcloud-python-sdk-examples/tree/main/classic-compute
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+__author__ = 'ryantiffany'
 import json
 import socket
 import threading
-
 import SoftLayer
 
 def check_port(ip, port, id, provision_user):
@@ -18,10 +33,9 @@ def check_port(ip, port, id, provision_user):
 
 win_hosts = []
 nix_hosts = []
-# Create SoftLayer client object
 client = SoftLayer.create_client_from_env()
 
-# Get all virtual guests
+# Get all bare metal servers and filter to only ones with public IPs
 bms = client['Account'].getHardware()
 filtered_bms = [s for s in bms if s.get('primaryIpAddress')]
 
